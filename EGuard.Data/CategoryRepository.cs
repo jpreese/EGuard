c0fa2cce40b4be2a862a1f04a913d99cbe38f046
John Reese <jpreese@mtu.edu>
2017-03-26 14:08:40 -0400

    initial commit

---
+++ EGuard.Data/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using EGuard.Data.Models;
+using System.Data;
+using Dapper;
+using System.Linq;
+
+namespace EGuard.Data
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IDbConnection _database;
+
+        public CategoryRepository(IDbConnection database)
+        {
+            _database = database;
+        }
+
+        public void Add(Category model)
+        {
+            _database.Execute("INSERT INTO Category(Name) VALUES(@Name)", new { Name = model.Name });
+        }
+
+        public void Delete(Category model)
+        {
+            _database.Execute("DELETE FROM Category WHERE Name = @Name", new { Name = model.Name });
+        }
+
+        public Category Get(Category model)
+        {
+            return _database.Query<Category>("SELECT Name FROM Category WHERE Name = @Name", new { Name = model.Name }).SingleOrDefault();
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _database.Query<Category>("SELECT Name FROM Category");
+        }
+    }
+}
---
+++ EGuard.Data/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using EGuard.Data.Models;
+using System.Collections.Generic;
+
+namespace EGuard.Data
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<Category> GetAllCategories();
+    }
+}
