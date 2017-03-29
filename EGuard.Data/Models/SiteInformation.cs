faff78b9f6b8c36d449c62b9cdc96b2f1eb3af55
John Reese <jpreese@mtu.edu>
2017-03-29 01:05:06 -0400

    Remove unused namespaces

--- EGuard.Data/Models/SiteInformation.cs
+++ EGuard.Data/Models/SiteInformation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EGuard.Data.Models
+﻿namespace EGuard.Data.Models
 {
     public class SiteInformation
     {
