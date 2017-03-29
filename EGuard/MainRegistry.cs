b806c8c865922ae45c19ca8b9d3a62a8751b7914
John Reese <jpreese@mtu.edu>
2017-03-29 01:05:33 -0400

    Add StructureMap registry class for startup project

---
+++ EGuard/MainRegistry.cs
@@ -0,0 +1,21 @@
+﻿using EGuard.Data;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace EGuard
+{
+    public class MainRegistry : Registry
+    {
+        public MainRegistry()
+        {
+            Scan(s =>
+            {
+                s.TheCallingAssembly();
+                s.LookForRegistries();
+                s.WithDefaultConventions();
+
+                s.AssemblyContainingType<DataRegistry>();
+            });
+        }
+    }
+}
