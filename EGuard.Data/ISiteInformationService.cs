db8c7877a98617504cad3724f92bb6f6f435294f
John Reese <jpreese@mtu.edu>
2017-03-29 01:04:55 -0400

    Add interface for SiteInformationService

---
+++ EGuard.Data/ISiteInformationService.cs
@@ -0,0 +1,10 @@
+﻿using EGuard.Data.Models;
+using System.Threading.Tasks;
+
+namespace EGuard.Data
+{
+    public interface ISiteInformationService
+    {
+        Task<SiteInformation> GetSiteInformation(string url);
+    }
+}
