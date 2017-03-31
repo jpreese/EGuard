93826c5a749e4bc55887533550103b8fd525c490
John Reese <jpreese@mtu.edu>
2017-03-31 18:21:34 -0400

    Initial commit

---
+++ EGuard.Data/ISiteVerificationService.cs
@@ -0,0 +1,10 @@
+﻿using EGuard.Data.Models;
+using System.Threading.Tasks;
+
+namespace EGuard.Data
+{
+    public interface ISiteVerificationService
+    {
+        Task<bool> Verify(Site site);
+    }
+}
---
+++ EGuard.Data/Services/SiteVerificationService.cs
@@ -0,0 +1,13 @@
+﻿using EGuard.Data.Models;
+using System.Threading.Tasks;
+
+namespace EGuard.Data.Services
+{
+    public class SiteVerificationService : ISiteVerificationService
+    {
+        public async Task<bool> Verify(Site site)
+        {
+            return false;
+        }
+    }
+}
