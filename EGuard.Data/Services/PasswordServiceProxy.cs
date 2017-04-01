9172e755ba4fab367510a75c46bd7f9ecd0c03d9
John Reese <jpreese@mtu.edu>
2017-04-01 17:14:39 -0400

    Initial commit

---
+++ EGuard.Data/Services/PasswordServiceProxy.cs
@@ -0,0 +1,25 @@
+﻿using EGuard.Data.Models;
+
+namespace EGuard.Data.Services
+{
+    public class PasswordServiceProxy
+    {
+        private readonly IPasswordService _passwordService;
+        private static Password _cache;
+
+        public PasswordServiceProxy(IPasswordService passwordService)
+        {
+            _passwordService = passwordService;
+        }
+
+        public Password GetPassword()
+        {
+            if(_cache == null)
+            {
+                _cache = _passwordService.GetPassword();
+            }
+
+            return _cache;
+        }
+    }
+}
