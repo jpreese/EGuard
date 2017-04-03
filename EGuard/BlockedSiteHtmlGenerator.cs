836cda8f8b47177cc10e30fabe08a33f3c6afe34
John Reese <jpreese@mtu.edu>
2017-04-03 12:55:33 -0400

    Initial commit

---
+++ EGuard/BlockedSiteHtmlGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace EGuard
+{
+    class BlockedSiteHtmlGenerator
+    {
+        public string GetHtml()
+        {
+            var uri = new Uri("pack://application:,,,/Resources/blocked.html");
+            var resourceStream = Application.GetResourceStream(uri);
+
+            using (var reader = new StreamReader(resourceStream.Stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
