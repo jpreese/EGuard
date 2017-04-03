0f4054a77565d119a09640cd6f23625b164b9c70
John Reese <jpreese@mtu.edu>
2017-04-03 15:26:10 -0400

    Initial commit

---
+++ EGuard/Rules/RuleChecker.cs
@@ -0,0 +1,22 @@
+﻿using EGuard.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EGuard.Rules
+{
+    public class RuleChecker
+    {
+        public bool CheckRules(Site site)
+        {
+            return GetRules().All(r => r.Check(site));
+        }
+
+        private IList<IRule> GetRules()
+        {
+            var rules = new List<IRule>();
+            rules.Add(new TimeRule());
+
+            return rules;
+        }
+    }
+}
