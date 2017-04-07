5e2c6d84968f6960328b66b7f0b64fb190e57078
John Reese <jpreese@mtu.edu>
2017-04-06 22:50:37 -0400

    Generate current key presses and processes

--- EGuard/Reporting/ReportGenerator.cs
+++ EGuard/Reporting/ReportGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using EGuard.Data;
 using EGuard.Data.Models;
+using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace EGuard.Reporting
@@ -18,16 +20,32 @@ namespace EGuard.Reporting
             var logs = _siteLogger.GetLogs();
             using (var sw = new StreamWriter(FileName))
             {
+                sw.WriteLine("======= Sites =======");
                 foreach (var log in logs)
                 {
                     sw.WriteLine(FormatReportLine(log));
                 }
+
+                sw.WriteLine("======= Keys =======");
+                sw.Write(FormatNewLines(MainWindow.ViewModel.KeyPresses));
+
+                sw.WriteLine("======= Processes =======");
+                foreach(var process in Process.GetProcesses())
+                {
+                    sw.WriteLine(process.ProcessName);
+                }
+
             }
         }
 
         private string FormatReportLine(Site site)
         {
             return string.Format("[{0}] {1}", site.Date, site.Url);
+        }
+
+        private string FormatNewLines(string keys)
+        {
+            return keys.Replace("\r", Environment.NewLine);
         }
     }
 }
