4cec2f049947441f85562f3c77e28d4c2da336f4
John Reese <jpreese@mtu.edu>
2018-03-28 07:34:01 -0400

    Initial commit

---
+++ EGuard.Data/SiteInformationService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EGuard.Data.Models;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace EGuard.Data
+{
+    public class SiteInformationService
+    {
+        public async Task<SiteInformation> GetSiteInfoAsJson(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "url", url }
+                };
+
+                var content = new FormUrlEncodedContent(values);
+                var request = await client.PostAsync("http://sitereview.bluecoat.com/rest/categorization", content);
+                var responseString = await request.Content.ReadAsStringAsync();
+
+                var category = CreateSiteInfoJson(responseString);
+
+                return category;
+            }
+        }
+
+        private SiteInformation CreateSiteInfoJson(string json)
+        {
+            var siteInfo = JsonConvert.DeserializeObject<SiteInformation>(json);
+            siteInfo.Categorization = RemoveAnchorTagFromCategory(siteInfo.Categorization);
+
+            return siteInfo;
+        }
+
+        private string RemoveAnchorTagFromCategory(string category)
+        {
+            return Regex.Match(category, @"<a [^>]*>(.*?)</a>").Groups[1].Value;
+        }
+    }
+}
---
+++ EGuard/ProxyFacade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace EGuard
+{
+    public class ProxyFacade
+    {
+        public static void Start()
+        {
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true);
+            ProxyServer.AddEndPoint(explicitEndPoint);
+
+            ProxyServer.BeforeRequest += OnRequest;
+
+            ProxyServer.Start();
+            ProxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+            ProxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+        }
+
+        private static void OnRequest(object sender, SessionEventArgs e)
+        {
+            var acceptHeader = e.ProxySession.Request.RequestHeaders.Find(h => h.Name == "Accept");
+            if(acceptHeader != null && acceptHeader.Value.Contains("text/html"))
+            {
+                Console.WriteLine(e.ProxySession.Request.Url);
+            }
+        }
+
+        private bool ShouldBlockWebsite(string url)
+        {
+            // send POST request to blue coat
+            // receive JSON
+            // parse/structure JSON
+            // get the category of website
+
+            // 1) check system time vs. time restriction in database
+            // 2) if category not found, block it
+            // 3) check blocked categories in database
+            // 4) if none of the above, allow
+
+            return false;
+        }
+    }
+}
