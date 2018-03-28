4584fc7bfe6f244546ce9216e30191984c762ca5
John Reese <jpreese@mtu.edu>
2018-03-28 07:33:26 -0400

    Remove unused namespaces and test driver code

--- EGuard/MainWindow.xaml.cs
+++ EGuard/MainWindow.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using EGuard.Data;
 
 namespace EGuard
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var c = new SiteInformationService();
+            var t = c.GetSiteInfoAsJson("http://www.reddit.com");
+
+
+
+            //ProxyFacade.Start();
         }
     }
 }
