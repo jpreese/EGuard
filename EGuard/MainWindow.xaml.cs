d295e98ecbf4cabf1cdf3deed83937df94ada9a2
John Reese <jpreese@mtu.edu>
2018-03-31 01:37:24 -0400

    Wire up the block and unblock events

--- EGuard/MainWindow.xaml.cs
+++ EGuard/MainWindow.xaml.cs
@@ -1,28 +1,48 @@
 ﻿using System.Windows;
 using StructureMap;
 using EGuard.Data;
+using EGuard.Data.Models;
 
 namespace EGuard
 {
     public partial class MainWindow : Window
     {
+        private readonly IContainer _container;
+        private readonly ICategoryRepository _categoryRepository;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _container = Container.For<MainRegistry>();
+            _categoryRepository = _container.GetInstance<CategoryRepository>();
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var container = Container.For<MainRegistry>();
+            var proxy = _container.GetInstance<ProxyFacade>();
 
-            var proxy = container.GetInstance<ProxyFacade>();
-            var categories = container.GetInstance<CategoryRepository>();
-
-            lstAllCategories.ItemsSource = categories.GetAllCategories();
-            lstBlockedCategories.ItemsSource = categories.GetBlockedCategories();
-            cboAssignableCategories.ItemsSource = categories.GetAllCategories();
+            lstAllCategories.ItemsSource = _categoryRepository.GetAllCategories();
+            lstBlockedCategories.ItemsSource = _categoryRepository.GetBlockedCategories();
+            cboAssignableCategories.ItemsSource = _categoryRepository.GetAllCategories();
 
             //proxy.Start();
+        }
+
+        private void btnUnblockCategory_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCategory = (Category)lstBlockedCategories.SelectedItem;
+            _categoryRepository.UnblockCategory(selectedCategory);
+
+            lstBlockedCategories.ItemsSource = _categoryRepository.GetBlockedCategories();
+        }
+
+        private void btnBlockCategory_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCategory = (Category)lstAllCategories.SelectedItem;
+            _categoryRepository.BlockCategory(selectedCategory);
+
+            lstBlockedCategories.ItemsSource = _categoryRepository.GetBlockedCategories();
         }
     }
 }
