a34b21aacd7ba3c1c8575c01a0650dc83f4eb088
John Reese <jpreese@mtu.edu>
2018-03-31 07:41:24 -0400

    Initial commit

---
+++ EGuard.Data/KeywordRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using EGuard.Data.Models;
+using System.Data;
+using Dapper;
+
+namespace EGuard.Data
+{
+    public class KeywordRepository : IKeywordRepository
+    {
+        private readonly IDbConnection _database;
+
+        public KeywordRepository(IDbConnection database)
+        {
+            _database = database;
+        }
+
+        public void Add(Keyword model)
+        {
+            _database.Execute("INSERT INTO Keyword(Description) VALUES(@Description)", new { Description = model.Description });
+        }
+
+        public void Delete(Keyword model)
+        {
+            _database.Execute("DELETE FROM Keyword WHERE Description = @Description", new { Description = model.Description });
+        }
+
+        public Keyword Get(Keyword model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Keyword> GetAllKeywords()
+        {
+            return _database.Query<Keyword>("SELECT Description FROM Keyword");
+        }
+    }
+}
