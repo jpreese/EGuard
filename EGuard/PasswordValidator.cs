95e653cd4a56afe11d4b7168c6c410754d0f437c
John Reese <jpreese@mtu.edu>
2018-04-01 03:24:22 -0400

    Initial commit

---
+++ EGuard/PasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace EGuard
+{
+    public class PasswordValidator
+    {
+        public bool Validate()
+        {
+            var passwordDialog = new PasswordDialog();
+
+            if (passwordDialog.ShowDialog() == true && passwordDialog.Result == "abc123")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
