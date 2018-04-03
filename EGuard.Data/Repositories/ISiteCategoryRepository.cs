44d5a74b1c88c98d7875c4b4f98174ac9dd617ca
John Reese <jpreese@mtu.edu>
2018-04-03 02:22:29 -0400

    Initial commit

---
+++ EGuard.Data/Repositories/ISiteCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using EGuard.Data.Models;
+using System.Collections.Generic;
+
+namespace EGuard.Data.Repositories
+{
+    public interface ISiteCategoryRepository : IRepository<SiteCategory>
+    {
+        IEnumerable<SiteCategory> GetPendingSites();
+    }
+}
