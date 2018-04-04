1666c5d568091c275260ed342c562a8f1ab258f6
John Reese <jpreese@mtu.edu>
2018-04-04 00:58:11 -0400

    Add additional rules to be processed

--- EGuard/Rules/RuleChecker.cs
+++ EGuard/Rules/RuleChecker.cs
@@ -1,4 +1,7 @@
 ﻿using EGuard.Data.Models;
+using EGuard.Data.Repositories;
+using EGuard.Data.Services;
+using EGuard.Emailing;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +9,22 @@ namespace EGuard.Rules
 {
     public class RuleChecker
     {
+        private readonly ISiteInformationService _siteInformationService;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly UnidentifiedCategoryMailer _unidentifiedCategoryMailer;
+        private readonly ISiteCategoryRepository _siteCategoryRepository;
+
+        public RuleChecker(ISiteInformationService siteInformationService, 
+            ICategoryRepository categoryRepository, 
+            UnidentifiedCategoryMailer unidentifiedCategoryMailer, 
+            ISiteCategoryRepository siteCategoryRepository)
+        {
+            _siteInformationService = siteInformationService;
+            _categoryRepository = categoryRepository;
+            _unidentifiedCategoryMailer = unidentifiedCategoryMailer;
+            _siteCategoryRepository = siteCategoryRepository;
+        }
+
         public bool CheckRules(Site site)
         {
             return GetRules().All(r => r.Check(site));
@@ -15,6 +34,8 @@ namespace EGuard.Rules
         {
             var rules = new List<IRule>();
             rules.Add(new TimeRule());
+            rules.Add(new BlockedCategoryRule(_siteInformationService, _categoryRepository));
+            rules.Add(new UnidentifiedCategoryRule(_siteInformationService, _unidentifiedCategoryMailer, _siteCategoryRepository));
 
             return rules;
         }
